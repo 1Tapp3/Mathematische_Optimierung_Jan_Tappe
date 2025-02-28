2c4b9d8ce8f9be8d42cac294d1df20e625e12296
1Tapp3 <117094744+1Tapp3@users.noreply.github.com>
2025-02-28 20:31:09 +0100

    Schnittstelle BO (test_BO.py noch anpassen)
    Einfügen von bereits gesammelten daten bezüglich einer optimierung können bei der Initialisierung mit übergeben werden.

--- BayesianOptimization.py
+++ BayesianOptimization.py
@@ -8,22 +8,27 @@ from SQP import SQP
 
 
 class BO(object):
-    def __init__(self):
+    def __init__(self, data_x: np.array = None, data_y: np.array = None):
         super().__init__()
+        self.data_x = data_x if data_x is not None else np.empty((0, 0))
+        self.data_y = data_y if data_y is not None else np.empty((0,))
+        assert self.data_x.shape[0] == self.data_y.shape[0]
 
     def Minimize(self, function: IDifferentiableFunction, iterations: int = 50) -> np.array:
 
         domain = function.domain
         d = domain._ambient_dimension
-        data_x = np.empty((0, d))
-        data_y = np.empty((0,))
+        if self.data_x.shape[0] == 0:
+            data_x = np.empty((0, d))   
+            data_y = np.empty((0,))    
+
         gp = GP(data_x=data_x, data_y=data_y)
         sqp = SQP()
 
         for step in range(iterations):
             # UCB with 2 sigma, added with 0 times the original function to get the domain
             acquisition_function = gp.PosteriorMean(
-            )-2*gp.PosteriorStandardDeviation() + 0*function
+            )-2*gp.PosteriorStandardDeviation() + 0*function    #why 2? wouldnt it be better to use another parameter to chose how much we want to weight the uncertainty?
 
             # new measurement point
             startingpoint = domain.point()
